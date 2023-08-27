626e14d73c1fa24957ee4e88804c364de7fbc609
PrayagN <Prayag.n.manojnair@gmail.com>
2023-08-27 10:44:45 +0530

    movie related crud operations completed

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -52,18 +52,194 @@ module.exports = {
 
   //@desc get movie by id
 
-  getMovieById :asyncHandler(async(req,res)=>{
+  getMovieById: asyncHandler(async (req, res) => {
     try {
-        // find movie by id  in database
-        const movie = await Movie.findById(req.params.id);
-        if(movie){
-            res.json(movie);
-        }else{
-            res.status(404);
-            throw new Error("Movie not found");
-        }
+      // find movie by id  in database
+      const movie = await Movie.findById(req.params.id);
+      if (movie) {
+        res.json(movie);
+      } else {
+        res.status(404);
+        throw new Error("Movie not found");
+      }
     } catch (error) {
-        res.status(400).json({message:error.message})
+      res.status(400).json({ message: error.message });
     }
-  })
+  }),
+
+  //@desc get top rated movies
+  getTopRatedMovies: asyncHandler(async (req, res) => {
+    try {
+      const movies = await Movie.find({}).sort({ rate: -1 });
+      res.json(movies);
+    } catch (error) {
+      res.status(400).json({ message: error.message });
+    }
+  }),
+
+  getRandomMovies: asyncHandler(async (req, res) => {
+    try {
+      const movies = await Movie.aggregate([{ $sample: { size: 8 } }]);
+      res.json(movies);
+    } catch (error) {
+      res.status(400).json({ message: error.message });
+    }
+  }),
+
+  createMovieReview: asyncHandler(async (req, res) => {
+    const { rating, comment } = req.body;
+    try {
+      const movie = await Movie.findById(req.params.id);
+      if (movie) {
+        const alreadyReviewed = movie.reviews.find(
+          (r) => r.userId.toString() === req.user._id.toString()
+        );
+
+        if (alreadyReviewed) {
+          res.status(400);
+          throw new Error("You already reviewed this movie");
+        }
+        const review = {
+          userName: req.user.fullName,
+          userId: req.user._id,
+          rating: Number(rating),
+          comment,
+        };
+        // push the new review to the reviews array
+        movie.reviews.push(review);
+        // increment the number of reviews
+        movie.numberOfReviews = movie.reviews.length;
+
+        // calculate the new rate
+
+        movie.rate =
+          movie.reviews.reduce((acc, item) => item.rating + acc, 0) /
+          movie.reviews.length;
+
+        await movie.save();
+
+        res.status(201).json({ message: "Review added" });
+      } else {
+        res.status(404);
+        throw new Error("Movie not found");
+      }
+    } catch (error) {
+      res.status(400).json({ message: error.message });
+    }
+  }),
+
+  /// Admin Controll ///
+
+  updateMovie: asyncHandler(async (req, res) => {
+    try {
+      const {
+        name,
+        desc,
+        image,
+        titleImage,
+        rate,
+        numberOfReviews,
+        category,
+        time,
+        language,
+        year,
+        casts,
+        video,
+      } = req.body;
+      // find movie by id in database
+
+      const movie = await Movie.findById(req.params.id);
+      if (movie) {
+        movie.name = name || movie.name;
+        movie.desc = desc || movie.desc;
+        movie.image = image || movie.image;
+        movie.titleImage = titleImage || movie.titleImage;
+        movie.rate = rate || movie.rate;
+        movie.numberOfReviews = numberOfReviews || movie.numberOfReviews;
+        movie.category = category || movie.category;
+        movie.time = time || movie.time;
+        movie.language = language || movie.language;
+        movie.year = year || movie.year;
+        movie.video = video || movie.video;
+        movie.casts = casts || movie.casts;
+
+        const updatedMovie = await movie.save();
+        res.status(201).json(updatedMovie);
+      } else {
+        res.status(404);
+        throw new Error("Movie not found");
+      }
+    } catch (error) {
+      res.status(400).json({ message: error.message });
+    }
+  }),
+
+  //@desc Delete Movie
+
+  deleteMovie: asyncHandler(async (req, res) => {
+    try {
+      const movie = await Movie.findById(req.params.id);
+      if (movie) {
+        await movie.deleteOne();
+        res.json({ message: "Movie removed" });
+      }
+      // if the movie is not found send 404 error
+      else {
+        res.status(404);
+        throw new Error("Movie not found");
+      }
+    } catch (error) {
+      res.status(400).json({ message: error.message });
+    }
+  }),
+
+  //@desc Create Movie
+
+  createMovie: asyncHandler(async (req, res) => {
+    try {
+      const {
+        name,
+        desc,
+        image,
+        titleImage,
+        rate,
+        numberOfReviews,
+        category,
+        time,
+        language,
+        year,
+        video,
+        casts,
+      } = req.body;
+      
+      // create a new movie
+      const movie = new Movie({
+        name,
+        desc,
+        image,
+        titleImage,
+        rate,
+        numberOfReviews,
+        category,
+        time,
+        language,
+        year,
+        video,
+        casts,
+        userId :req.user._id
+      })
+
+      // save the movie in database
+
+      if(movie){
+        const createdMovie = await movie.save();
+        res.status(201).json(createdMovie)
+      }else{
+        res.status(400);
+        throw new Error("Invalid movie data");
+      }
+    } catch (error) {
+      res.status(400).json({ message: error.message });
+    }
+  }),
 };
--- routes/movieRoutes.js
+++ routes/movieRoutes.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const router = express();
-const {importMovies, getMovies} = require('../controllers/movieController');
+const {importMovies, getMovies, getMovieById, getTopRatedMovies, getRandomMovies, createMovieReview, updateMovie, deleteMovie, createMovie} = require('../controllers/movieController');
 const {userProtect,admin} = require('../middleware/Auth');
 
 router.post('/import',importMovies);
 router.get('/',getMovies)
+router.get('/:id',getMovieById)
+router.get('/rated/top',getTopRatedMovies)
+router.get('/random/all',getRandomMovies)
 
+
+router.post('/:id/reviews',userProtect,createMovieReview)
+router.put("/:id",userProtect,admin,updateMovie)
+router.delete("/:id",userProtect,admin,deleteMovie);
+router.post('/',userProtect,admin,createMovie)
 module.exports = router
